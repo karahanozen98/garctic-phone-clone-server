341be20b0555fe5140c7db428168dfad8c35c89f
Karahan Özen <karahan.ozen@adesso.com.tr>
2024-10-20 14:00:46 +0300

    remove websocket

--- out/index.js
+++ out/index.js
@@ -15,7 +15,6 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: { origin: process.env.CLIENT_URL, credentials: true },
-    transports: ["websocket"],
 });
 connectMongo();
 io.on("connection", (socket) => {
--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,6 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: { origin: process.env.CLIENT_URL, credentials: true },
-  transports: ["websocket"],
 });
 
 connectMongo();
