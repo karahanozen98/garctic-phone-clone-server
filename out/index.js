52801210446e00170a59eb0c18e41c5df43145cd
Karahan Özen <karahan.ozen@adesso.com.tr>
2024-10-20 13:54:16 +0300

    Add socket io transports

--- out/index.js
+++ out/index.js
@@ -15,6 +15,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: { origin: process.env.CLIENT_URL, credentials: true },
+    transports: ["websocket"],
 });
 connectMongo();
 io.on("connection", (socket) => {
